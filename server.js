8e7ee633707a423ef21e28481d0e5aef2ca90ceb
piggelini <sigridwidfeldt@gmail.com>
2022-05-11 15:55:43 +0200

    create.ejs form created

--- server.js
+++ server.js
@@ -11,8 +11,38 @@ import { MongoClient, ObjectId } from 'mongodb';
 const client = new MongoClient('mongodb://localhost:27017');
 await client.connect();
 
+const db = client.db('knitting-club');
+const memberships = db.collection('members');
+
 app.get('/', async (req, res) => {
     res.render('homepage');
+});
+
+
+
+app.get('/members', async (req, res) => {
+    const members = await memberships.find({}).toArray();
+    res.render('members', { members });
+});
+
+app.get('/member/:id', async (req, res) => {
+    const member = await memberships.findOne({ _id: ObjectId(req.params.id) });
+    res.render('member', {
+        name: member.name,
+        email: member.email,
+        number: member.number,
+        joined: member.joined,
+        level: member.level
+    });
+});
+
+app.get('/members/create', async (req, res) => {
+    res.render('create');
+});
+
+app.post('/members/create', async (req, res) => {
+    await memberships.insertOne(req.body);
+    res.redirect('/members');
 });
 
 
